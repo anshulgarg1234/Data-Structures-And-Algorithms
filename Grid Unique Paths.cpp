bc2d124dd7f556e976ceae4a51019c2603c5159d
Yash Chaudhari <yash56244@gmail.com>
2020-09-03 15:35:55 +0530

    Add files via upload

---
+++ Grid Unique Paths.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int brute(int i, int j, int n, int m)
+{
+    if(i == n - 1 && j == m - 1){
+        return 1;
+    }
+    if(i >= n || j >= m){
+        return 0;
+    }
+    return brute(i + 1, j, n, m) + brute(i, j + 1, n, m);
+}
+int optimal(int m, int n) {
+        int N = n + m - 2;
+        int r = m - 1;
+        long long int res = 1;
+        for(int i = 1; i <= r; i++){
+            res = res * (N - r + i)/i;
+        }
+        return res;
+    }
+int main()
+{
+    cout<<"Brute : "<<brute(0,0,3,3)<<endl; //Using recursion count all the ways.
+    // Method 2 is using DP(memoization).
+    cout<<"Optimal : "<<optimal(5,5)<<endl; //By observation we return the combinations.
+    return 0;
+}
